2ecc2ab516e38d84e1b3f65a19972e6c77c69288
giangoma <144329931+giangoma@users.noreply.github.com>
2023-12-31 18:44:33 +0400

    vendingmachine.py

---
+++ vendingmachine.py
@@ -0,0 +1,163 @@
+print(""" \t \t \t \t$$$$$$\  $$$$$$\ $$\  $$$$$$\        $$\    $$\ $$$$$$$$\ $$\   $$\ $$$$$$$\  $$$$$$\ $$\   $$\  $$$$$$\        $$\      $$\  $$$$$$\   $$$$$$\  $$\   $$\ $$$$$$\ $$\   $$\ $$$$$$$$\ 
+\t \t \t \t$$  __$$\ \_$$  _|$  |$$  __$$\       $$ |   $$ |$$  _____|$$$\  $$ |$$  __$$\ \_$$  _|$$$\  $$ |$$  __$$\       $$$\    $$$ |$$  __$$\ $$  __$$\ $$ |  $$ |\_$$  _|$$$\  $$ |$$  _____|
+\t \t \t \t$$ /  \__|  $$ |  \_/ $$ /  \__|      $$ |   $$ |$$ |      $$$$\ $$ |$$ |  $$ |  $$ |  $$$$\ $$ |$$ /  \__|      $$$$\  $$$$ |$$ /  $$ |$$ /  \__|$$ |  $$ |  $$ |  $$$$\ $$ |$$ |      
+\t \t \t \t$$ |$$$$\   $$ |      \$$$$$$\        \$$\  $$  |$$$$$\    $$ $$\$$ |$$ |  $$ |  $$ |  $$ $$\$$ |$$ |$$$$\       $$\$$\$$ $$ |$$$$$$$$ |$$ |      $$$$$$$$ |  $$ |  $$ $$\$$ |$$$$$\    
+\t \t \t \t$$ |\_$$ |  $$ |       \____$$\        \$$\$$  / $$  __|   $$ \$$$$ |$$ |  $$ |  $$ |  $$ \$$$$ |$$ |\_$$ |      $$ \$$$  $$ |$$  __$$ |$$ |      $$  __$$ |  $$ |  $$ \$$$$ |$$  __|   
+\t \t \t \t$$ |  $$ |  $$ |      $$\   $$ |        \$$$  /  $$ |      $$ |\$$$ |$$ |  $$ |  $$ |  $$ |\$$$ |$$ |  $$ |      $$ |\$  /$$ |$$ |  $$ |$$ |  $$\ $$ |  $$ |  $$ |  $$ |\$$$ |$$ |      
+\t \t \t \t\$$$$$$  |$$$$$$\     \$$$$$$  |         \$  /   $$$$$$$$\ $$ | \$$ |$$$$$$$  |$$$$$$\ $$ | \$$ |\$$$$$$  |      $$ | \_/ $$ |$$ |  $$ |\$$$$$$  |$$ |  $$ |$$$$$$\ $$ | \$$ |$$$$$$$$\ 
+\t \t \t \t \______/ \______|     \______/           \_/    \________|\__|  \__|\_______/ \______|\__|  \__| \______/       \__|     \__|\__|  \__| \______/ \__|  \__|\______|\__|  \__|\________|""")
+
+print ("""\t \t \t \t---------------------------------------------------------------Welcome to Gi's Vending Machine! We offer a variety of items listed below.----------------------------------------------
+ \t \t \t \t-----------------------------------------------------------------------Type in the code of your order to confirm------------------------------------------------------------------------""")
+
+print(""" \t \t \t \t____________________________________________                          
+ \t \t \t \t|############################################|                                                          ~SNACKS~         
+ \t \t \t \t|#|                           |##############|                                =Name=                     =Price=         =Code=     =Stock=
+ \t \t \t \t|#|  =====  ..--''`  |~~``|   |##|````````|##|                                >Chocolate Chip Cookies-----3AED-------------A1----------5         
+ \t \t \t \t|#|  |   |  \     |  :    |   |##| Exact  |##|                                >Chocolate-Filled Croissant-5AED-------------A2----------4 
+ \t \t \t \t|#|  |___|   /___ |  | ___|   |##| Change |##|                                >Vanilla Biscuit------------3AED-------------A3----------6
+ \t \t \t \t|#|  /=__\  ./.__\   |/,__\   |##| Only   |##|                                >Tuna Sandwich--------------7AED-------------A4----------4
+ \t \t \t \t|#|  \__//   \__//    \__//   |##|________|##|                                >Egg & Mayo Sandwich--------7AED-------------A5----------2
+ \t \t \t \t|#|===========================|##############|                                >Flaming Hot Cheetos--------3AED-------------A6----------3
+ \t \t \t \t|#|```````````````````````````|##############|                                
+ \t \t \t \t|#| =.._      +++     //////  |##############|                                                       ~COLD BEVERAGES~ 
+ \t \t \t \t|#| \/  \     | |     \    \  |#|`````````|##|                                =Name=                     =Price=         =Code=     =Stock=
+ \t \t \t \t|#|  \___\    |_|     /___ /  |#| _______ |##|                                >Bottled Water--------------1AED-------------B1----------8
+ \t \t \t \t|#|  / __\\  /|_|\   // __\    |#| |A|1|2| |##|                                >Peach Iced Tea-------------5AED-------------B2----------6
+ \t \t \t \t|#|  \__//-  \|_//   -\__//   |#| |B|3|4| |##|                                >Iced Mocha Coffee----------5AED-------------B3----------5
+ \t \t \t \t|#|===========================|#| |C|5|6| |##|                                >Chocolate Milk-------------3AED-------------B4----------4
+ \t \t \t \t|#|```````````````````````````|#| ``````` |##|                                >Fizzy Drink----------------3AED-------------B5----------5
+ \t \t \t \t|#| ..--    ______   .--._.   |#|[=======]|##|                                >Fruit Cocktail-------------3AED-------------B6----------2
+ \t \t \t \t|#| \   \   |    |   |    |   |#|  _   _  |##|
+ \t \t \t \t|#|  \___\  : ___:   | ___|   |#| ||| ( ) |##|                                                        ~HOT BEVERAGES~ 
+ \t \t \t \t|#|  / __\  |/ __\   // __\   |#| |||  `  |##|                                =Name=                     =Price=         =Code=     =Stock=
+ \t \t \t \t|#|  \__//   \__//  /_\__//   |#|  ~      |##|                                >Hot Coffee-----------------3AED-------------C1----------3
+ \t \t \t \t|#|===========================|#|_________|##|                                >Tea with Honey-------------3AED-------------C2----------4
+ \t \t \t \t|#|```````````````````````````|##############|                                >Karak Chai-----------------2AED-------------C3----------5
+ \t \t \t \t|############################################|
+ \t \t \t \t|#|||||||||||||||||||||||||||||####```````###|                               HAPPY PURCHASING! PLEASE START BY INPUTTING YOUR MONEY (MIN. 2AED)
+ \t \t \t \t|#||||||||||||PUSH|||||||||||||####\|||||/###|                                ______________________________________________________________
+ \t \t \t \t|############################################|                               |                                                              |
+ \t \t \t \t\\\\\\\\\\\\\\\\\\\\\\///////////////////////                                           |                                                              |
+  \t \t \t \t|________________________________|CR98|___|                                  ----------------------------------------------------------------
+
+""")
+
+#Entering the money
+while True:
+    try:
+        money = int(input("Enter your cash amount: "))
+        if money < 2:
+            print("Sorry. The money you have inserted is insufficient. Please try again.")
+        else:
+            print("""
+                 \t \t \t \t \t    ╔╦╗╔═╗╔╗╔╔═╗╦ ╦  ╦═╗╔═╗╔═╗╔═╗╦╦  ╦╔═╗╔╦╗
+                 \t \t \t \t \t    ║║║║ ║║║║║╣ ╚╦╝  ╠╦╝║╣ ║  ║╣ ║╚╗╔╝║╣  ║║       
+                 \t \t \t \t \t    ╩ ╩╚═╝╝╚╝╚═╝ ╩   ╩╚═╚═╝╚═╝╚═╝╩ ╚╝ ╚═╝═╩╝
+                """)
+            print(f"\t \t \t \t \t \t Your current balance is {money}AED. Time to purchase your item!")
+            break
+    except ValueError:
+        print("Invalid input. Please try again.")
+
+
+
+#Inventory
+inventory = {
+            'A1': {'name': 'Chocolate Chip Cookies', 'price': 3, 'stock': 5},
+            'A2': {'name': 'Chocolate-Filled Croissant', 'price': 5, 'stock': 4},
+            'A3': {'name': 'Vanilla Biscuit', 'price': 3, 'stock': 6},
+            'A4': {'name': 'Tuna Sandwich', 'price': 7, 'stock': 4},
+            'A5': {'name': 'Egg & Mayo Sandwich', 'price': 7, 'stock': 2},
+            'A6': {'name': 'Flaming Hot Cheetos', 'price': 3, 'stock': 3},
+            'B1': {'name': 'Bottled Water', 'price': 1, 'stock': 8},
+            'B2': {'name': 'Peach Iced Tea', 'price': 5, 'stock': 6},
+            'B3': {'name': 'Iced Mocha Coffee', 'price': 5, 'stock': 5},
+            'B4': {'name': 'Chocolate Milk', 'price': 3, 'stock': 4},
+            'B5': {'name': 'Fizzy Drink', 'price': 3, 'stock': 5},
+            'B6': {'name': 'Fruit Cocktail', 'price': 3, 'stock': 2},
+            'C1': {'name': 'Hot Coffee', 'price': 3, 'stock': 3},
+            'C2': {'name': 'Tea with Honey', 'price': 3, 'stock': 4},
+            'C3': {'name': 'Karak Chai', 'price': 2, 'stock': 5},
+        }
+
+# Code Input
+while True:
+    item_code = input("\t \t \t \t \t Enter the code of the item you want to purchase (Type Q to quit): ").upper()
+
+    if item_code == "Q":
+        print("""
+            \t \t \t \t \t \t \t \t \t \t \t   ╔═╗╔═╗╔═╗╔╦╗╔╗ ╦ ╦╔═╗
+            \t \t \t \t \t \t \t \t \t \t \t   ║ ╦║ ║║ ║ ║║╠╩╗╚╦╝║╣ 
+            \t \t \t \t \t \t \t \t \t \t \t   ╚═╝╚═╝╚═╝═╩╝╚═╝ ╩ ╚═╝
+        """)
+        if money == 0:
+            print(f"\t \t \t \t \t \t \t                  Thanks for visiting!")
+        else:
+            print(f"\t \t \t \t \t \t \t                  Your {money}AED has also been refunded. Thanks for visiting!")
+        break
+
+    if item_code in inventory:
+        item = inventory[item_code]
+        item_price = item['price']
+
+        if item['stock'] > 0 and money >= item_price:
+            print(f"Dispensing {item['name']}...")
+            print("""
+                \t \t \t \t \t \t \t           ╔═╗╦═╗╔╦╗╔═╗╦═╗  ╦═╗╔═╗╔═╗╔═╗╦╦  ╦╔═╗╔╦╗
+                \t \t \t \t \t \t \t           ║ ║╠╦╝ ║║║╣ ╠╦╝  ╠╦╝║╣ ║  ║╣ ║╚╗╔╝║╣  ║║
+                \t \t \t \t \t \t \t           ╚═╝╩╚══╩╝╚═╝╩╚═  ╩╚═╚═╝╚═╝╚═╝╩ ╚╝ ╚═╝═╩╝            
+            """)
+            print(f"\t \t \t \t \t \t \t                                  Dispensing {item['name']}...")
+
+            # Deduct the cost from the money inserted
+            money -= item_price
+
+            # Update the stock of the selected item
+            item['stock'] -= 1
+            print(f"\t \t \t \t \t \t \t                             Your {item['name']} has been dispensed.")
+            if money == 0:
+                print(f"\t \t \t \t \t \t \t                             Thanks for visiting!")
+            else:
+                print(f"\t \t \t \t \t \t \t                               Your remaining balance is {money}AED.")
+
+            # Recommend a random item
+            import random
+            available_items = [code for code, item in inventory.items() if item['stock'] > 0]
+            if available_items:
+                random_item_code = random.choice(available_items)
+                random_item_name = inventory[random_item_code]['name']
+                print(f"\t \t \t \t \t \t \t                          We also recommend trying {random_item_name}.")
+            else:
+                print("\t \t \t \t \t \t \t                             Apologies, all items are currently out of stock.")
+        elif item['stock'] == 0:
+            print("""
+\t \t \t \t \t \t \t                                ╔═╗╦ ╦╔╦╗  ╔═╗╔═╗  ╔═╗╔╦╗╔═╗╔═╗╦╔═
+\t \t \t \t \t \t \t                                ║ ║║ ║ ║   ║ ║╠╣   ╚═╗ ║ ║ ║║  ╠╩╗
+\t \t \t \t \t \t \t                                ╚═╝╚═╝ ╩   ╚═╝╚    ╚═╝ ╩ ╚═╝╚═╝╩ ╩
+""")
+            print("\t \t \t \t \t \t \t                           Sorry, the selected item is out of stock.")
+        else:
+            print("Insufficient funds. Please insert more money.")
+    else:
+        print("Code invalid. Please enter a valid code.")
+
+    # Ask if the user wants to buy more items
+    buy_more = input("\t \t \t \t \t \t \t               Do you want to buy more items? (Type Y for Yes, N for No): ").upper()
+    while buy_more not in ["Y", "N"]:
+        print("\t \t \t \t \t \t \t                       Invalid input. Please type Y for Yes, N for No.")
+        buy_more = input("\t \t \t \t \t \t \t               Do you want to buy more items? (Type Y for Yes, N for No): ").upper()
+
+    if buy_more == "N":
+        print("""
+            \t \t \t \t \t                                             ╔═╗╔═╗╔═╗╔╦╗╔╗ ╦ ╦╔═╗
+            \t \t \t \t \t                                             ║ ╦║ ║║ ║ ║║╠╩╗╚╦╝║╣ 
+            \t \t \t \t \t                                             ╚═╝╚═╝╚═╝═╩╝╚═╝ ╩ ╚═╝
+        """)
+        if money == 0:
+            print(f"\t \t \t \t \t \t \t                                     Thanks for visiting!")
+        else:
+            print(f"\t \t \t \t \t \t \t                  Your {money}AED has also been refunded. Thanks for visiting!")
+        break
+
+
